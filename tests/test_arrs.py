fea6cd2668d8fc82913102ea47e48b80d29333b4
Andrew Skow <skovorodnikov11@gmail.com>
2023-10-11 12:50:48 +0300

    Name                 Stmts   Miss  Cover ---------------------------------------- tests/__init__.py        0      0   100% tests/test_arrs.py      24      0   100% utils/__init__.py        0      0   100% utils/arrs.py           15      0   100% ---------------------------------------- TOTAL                   39      0   100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,7 +7,27 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([], 0), None)
+        self.assertEqual(arrs.get([], -5), None)
+        self.assertEqual(arrs.get([1, 2, 3], -5), None)
+        self.assertEqual(arrs.get([1,2,3,5,8], 4), 8)
+
+
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 0), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1, -2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1, -2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2,3])
+
+
+    def test_slice_new(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 890),[2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1234, 890), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 0, 0), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 0), [])
+        self.assertEqual(arrs.my_slice([], -1, 0), [])
+
